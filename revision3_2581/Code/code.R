ce43b768ab6216c10c28e06ec2e751dad320bc59
stitam <stirling.tamas@gmail.com>
2020-03-21 14:49:58 +0000

    Updated code in the article

--- revision3_2581/Code/code.R
+++ revision3_2581/Code/code.R
@@ -1,16 +1,14 @@
 # -------------------------------------------------------------------------
-# Supplemental R Code to reproduce the results of
-# Szöcs, Schäfer. webchem: An R Package to Retrieve Chemical Information
-#   from the Web
+# Supplemental R Code to reproduce the results of Szöcs, Stirling, Schäfer. 
+# webchem: An R Package to Retrieve Chemical Information from the Web
 # ------------------------------------------------------------------------
 
 
 # Install webchem from CRAN -----------------------------------------------
 #install.packages("webchem")
 
-
 # load packages -----------------------------------------------------------
-#library("webchem")
+library("webchem")
 library("ggplot2")
 library("testthat")
 
@@ -42,57 +40,46 @@ head(etox_cas)
 
 # query other identifiers from other resources
 
+# get_cid() returned the PubChem ID of sodium when the query was NA. This was
+# fixed in the dev version. To install the dev version:
+
+library(devtools)
+install_github("ropensci/webchem")
+
 # query SMILES from PubChem
 cids <- get_cid(etox_cas)
 pc_data <- pc_prop(cids, properties = "CanonicalSMILES")
 pc_smiles <- smiles(pc_data)
 
-# ChemSpider needs an API key.
-# This is a webchem specific token.
-# Please use it only for reproduction / testing.
-token <- "37bf5e57-9091-42f5-9274-650a64398aaf"
-# query InChiKey from ChemSpider
-#csids <- get_csid(pc_smiles, from = "smiles", apikey = apikey)
-#cs_inchikey <- cs_convert(csids, from = "csid", to = "inchikey", apikey = apikey)
+# ChemSpider needs an API key and RSC explicitly asks users to keep the key
+# secure. In order to test these functions, please register for an API key.
+csids <- get_csid(pc_smiles, from = "smiles", apikey = apikey)
+cs_inchikey <- cs_convert(csids$csid, from = "csid", to = "inchikey",
+                          apikey = apikey)
+
+
 
 # combine into single data.frame
-#res <- data.frame(name = subs, cas = etox_cas, smiles = pc_smiles,
-#                  cid = pc_data$CID, inchikey = cs_inchikey,
-#                  csid = cs_data$csid,
-#                  stringsAsFactors = FALSE)
-#head(res)
-
-# Automatic tests to test if results are reproduced correctly
-test_that("Use Case 1: Query identifiers",{
-
-  expect_equal(names(etox_cas), c("8932", "8494", NA, "8397", "7240", "7331"))
-  expect_equal(unname(etox_cas), c("105-67-9", "1570-64-5", NA, "1912-24-9",
-                                   "71-43-2", "6190-65-4"))
-  expect_is(cids, "list")
-  expect_length(cids, 6)
-
-  expect_is(pc_data, c("pc_prop", "data.frame"))
-  expect_dim(pc_data, c(6,2))
-
-  expect_is(pc_smiles, "character")
-  expect_length(pc_smiles, 6)
-
-})
-
+ res <- data.frame(name = subs,
+                   cas = etox_cas,
+                   smiles = pc_smiles,
+                   cid = pc_data$CID,
+                   inchikey = cs_inchikey,
+                   csid = csids$csid,
+                   stringsAsFactors = FALSE,
+                   row.names = NULL)
+ head(res)
 
 # Use Case II: Toxicity of different pesticide groups ---------------------
+
 # query the pesticide compendium using CAS-numbers
-aw_data <- aw_query(lc50$cas[1:3], type = "cas")
+aw_data <- aw_query(lc50$cas, type = "cas")
 
 # shows internal structure of the data-object
 str(aw_data[[1]])
 # extract chemical group from list
 igroup <- sapply(aw_data, function(y) y$subactivity[1])
 igroup[1:3]
-
-aw_data <- aw_query(lc50$cas, type = "cas")
-igroup <- sapply(aw_data, function(y) unname(y["subactivity"][1]))
-
 
 # cleanup
 lc50$type <- ifelse(grepl("carbamat", igroup), "Carbamates",
@@ -115,36 +102,13 @@ p <- ggplot(lc50, aes(x = type, y = value)) +
   theme(text = element_text(size = 16))
 p
 
-# Automatic tests to test if results are reproduced correctly
-test_that("Use Case II: Toxicity of different pesticide groups",{
-
-  expect_equal(names(igroup[1:3]), c("50-29-3", "52-68-6", "55-38-9"))
-  expect_equal(unname(igroup[1:3]), c("organichlorine insecticides",
-                                      "phosphonate insecticides",
-                                      "phenyl organothiophosphate
-                                      insecticides"))
-
-})
-
-
 # Use Case III: Name 10 most toxic chemicals ------------------------------
-lc50_3 <- lc50[ order(lc50$value), ][1:3, ]
-# query ChEBI names from CAS
-lite <- get_chebiid(lc50_3$cas, match = 'best')
-lite <- do.call(rbind, lite) # bind to data.frmae
-lite$cas <- rownames(lite)
-# query ChEBI complete entity
-comp <- chebi_comp_entity(lite$chebiid) # use ChEBI ids to query the service
-comp <- do.call(rbind, lapply(comp, `[[`, "parents")) # extract parents list and bind them to a data.frame
-comp$chebiid <- gsub('\\.[0-9]+', '', rownames(comp))
-comp <- comp[ comp$type == "has role", ] # refine to chemical roles
-role <- aggregate(chebiName ~ chebiid, data = comp, FUN = function(x) paste0(x, collapse = ', '))
-role <- merge(lite[ names(lite) %in% c('cas', 'chebiid', 'chebiasciiname') ], role, by = 'chebiid')
-setNames(role, c('chebiid', 'name', 'cas', 'roles'))
 
-test_that("Use Case III: Name 10 most toxic chemicals",{
-
-})
+cas_rns <- lc50[order(lc50$value)[1:3],"cas"]
+chebiids <- get_chebiid(cas_rns)
+#chebiids <- do.call(rbind, chebiids)
+comp <- chebi_comp_entity(chebiids$chebiid)
+pars <- lapply(comp, function(x) with(x, parents[parents$type == "has role",]))
 
 # Use Case IV: Querying partitioning coefficients -------------------------
 # query PubChem DATABASE
@@ -172,16 +136,6 @@ p <- ggplot(lc50, aes(x = logp, y = value)) +
   theme_bw()
 p
 
-test_that("Use Case IV: Querying partitioning coefficients",{
-  expect_is(cid, "integer")
-  expect_is(pc_data, c("pc_prop", "data.frame"))
-  expect_is(lc50$logp, "numeric")
-  expect_length(lc50$logp[!is.na(lc50$logp)], 118)
-  expect_length(lc50$logp[is.na(lc50$logp)], 6)
-  expect_equal(round(coef(mod),2), c(2.86, -0.36))
-  expect_equal(round(summary(mod$sigma), 2), 1.47)
-})
-
 # Use Case V: Regulatory information --------------------------------------
 # search EQS from ETOX using the already queried ETOX_IDs
 eqs <- etox_targets(ids$etoxid)
@@ -203,28 +157,6 @@ ids$mac <- sapply(eqs, function(y){
 jagst_eqs <- merge(jagst, mac, by.x = "substance", by.y = "query")
 head(jagst_eqs)
 
-test_that("Use Case V: Regulatory information",{
-  expect_is(mac, "data.frame")
-  expect_equal(dim(mac), c(6,3))
-  expect_equal(names(mac), c("etoxid","query","mac"))
-  expect_equal(mac$etoxid, c(8397, 7240, 8836, 7442, 7571, 8756))
-  expect_equal(mac$query, c("Atrazin", "Benzol", "Irgarol", "Isoproturon",
-                            "Simazin", "Terbutryn"))
-  expet_equal(mac$mac, c(2.000, 50.000, 0.016, 1.000, 4.000, 0.034))
-  expect_is(jagst_eqs, "data.frame")
-  expect_equal(names(jagst_eqs), c("substance", "date", "value", "qual",
-                                   "etoxid", "mac"))
-  expect_equal(head(jagst_eqs$substance), rep("Atrazin", times = 6))
-  expect_equal(head(jagst_eqs$date), as.Date(c("2013-09-10", "2013-10-08",
-                                               "2013-03-26", "2013-04-23",
-                                               "2013-06-18", "2013-07-16")))
-  expect_equal(head(jagst_eqs$value, c(0.0068, 0.0072, 0.0040, 0.0048, 0.0048,
-                                       0.0052)))
-  expect_equal(head(jagst_eqs$qual), rep("=", times = 6))
-  expect_equal(head(jagst_eqs$etoxid), rep(8397, times = 6))
-  expect_equal(head(jagst_eqs$mac), rep(2, times = 6))
-})
-
 # Utilitymac functions -------------------------------------------------------
 # simple formatting check
 is.inchikey("BQJCRHHNABKAKU-KBQPJGBKS-AN")
@@ -233,10 +165,4 @@ is.cas("64-17-6")
 
 # or using ChemSpider
 is.inchikey("BQJCRHHNABKAKU-KBQPJGBKSA-5", type = "chemspider")
-
-test_that("Utility functions",{
-  expect_false(is.inchikey("BQJCRHHNABKAKU-KBQPJGBKS-AN"))
-  expect_false(is.cas("64-17-6"))
-  expect_false(is.inchikey("BQJCRHHNABKAKU-KBQPJGBKSA-5", type = "chemspider"))
-})
 
